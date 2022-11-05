6653fc9dc90080b161b8c15f88bfe13a3dcf203e
Thawat09 <97440224+Thawat09@users.noreply.github.com>
2022-11-06 04:45:23 +0700

    65/11/06
    65/11/06

---
+++ public/js/search.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInputSearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 1; i < tr.length; i++) {
+        tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td");
+        for (var j = 0; j < td.length; j++) {
+            cell = tr[i].getElementsByTagName("td")[j];
+            if (cell) {
+                if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    break;
+                }
+            }
+        }
+    }
+}
