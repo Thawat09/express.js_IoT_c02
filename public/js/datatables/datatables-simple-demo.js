f3367f1a0225fbfeb628a8826a5f59c0a7c3876d
Thawat09 <97440224+Thawat09@users.noreply.github.com>
2023-01-18 22:49:21 +0700

    66/01/18
    66/01/18

--- public/js/datatables/datatables-simple-demo.js
+++ public/js/datatables/datatables-simple-demo.js
@@ -1,9 +1,13 @@
-window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
+// window.addEventListener('DOMContentLoaded', event => {
+//     // Simple-DataTables
+//     // https://github.com/fiduswriter/Simple-DataTables/wiki
 
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
-    }
+//     const datatablesSimple = document.getElementById('datatablesSimple');
+//     if (datatablesSimple) {
+//         new simpleDatatables.DataTable(datatablesSimple);
+//     }
+// });
+
+$(document).ready(function () {
+    $('#example').DataTable();
 });
